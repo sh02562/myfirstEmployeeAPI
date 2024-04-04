42410fb3854f772df54a6b04e08bf951763e4d78
sh02562 <shweta02562@gmail.com>
2024-04-04 13:43:49 +0530

    commit again

---
+++ EmpFullStack/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using EmpFullStack.Data;
+using EmpFullStack.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmpFullStack.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : Controller
+    {
+        private readonly FullstackDbContext _fullstackDbContext;
+        public EmployeeController(FullstackDbContext fullstackDbContext)
+        {
+            this._fullstackDbContext=fullstackDbContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var employees = await _fullstackDbContext.Employees.ToListAsync();
+
+            return Ok(employees);
+        }
+        [HttpPost]
+       
+
+public async Task<IActionResult> AddEmployee([FromBody] Emp empRequest )
+        {empRequest.Id = Guid.NewGuid();
+            await _fullstackDbContext.Employees.AddAsync(empRequest);
+            await _fullstackDbContext.SaveChangesAsync();
+            return Ok(empRequest);
+        }
+    }
+}
