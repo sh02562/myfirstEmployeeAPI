988a3ae90e210f25b21f898b235deafb64053000
sh02562 <shweta02562@gmail.com>
2024-04-04 12:47:44 +0530

    first api project

---
+++ EmpFullStack/Data/FullstackDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EmpFullStack.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace EmpFullStack.Data
+{
+    public class FullstackDbContext : DbContext
+
+    {
+        public FullstackDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Emp> Employees { get; set; }
+
+        
+    }
+}
